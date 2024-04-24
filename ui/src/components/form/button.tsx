ec5dc6bfdd721e8c1d128b69382ff8e3e2c57c96
Zak Nesler <zak@nesler.dev>
2024-04-24 09:32:43 -0400

    no shrink

--- ui/src/components/form/button.tsx
+++ ui/src/components/form/button.tsx
@@ -5,7 +5,7 @@ type Props = ParentProps<JSX.IntrinsicElements['button']>;
 export const Button = ({ children, ...props }: Props) => (
   <button
     {...props}
-    class="appearance-none rounded-lg bg-gray-500 px-4 py-2 text-white transition duration-100 hover:bg-gray-800"
+    class="shrink-0 appearance-none rounded-lg bg-gray-500 px-4 py-2 text-white transition duration-100 hover:bg-gray-800"
     classList={{ [String(props.class)]: !!props.class }}
   >
     {children}
