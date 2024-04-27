0467489b534fd3e92f937686678f1332c790becb
Zak Nesler <zak@nesler.dev>
2024-04-27 11:59:16 -0400

    rm format

--- crates/blend-db/src/repo/feed.rs
+++ crates/blend-db/src/repo/feed.rs
@@ -1,8 +1,6 @@
 use crate::{error::DbResult, model};
 use chrono::{DateTime, Utc};
 
-const COLUMNS: &str = r#"uuid, url, title, published_at, updated_at"#;
-
 pub struct FeedRepo {
     ctx: blend_context::Context,
 }
@@ -20,18 +18,14 @@ impl FeedRepo {
     }
 
     pub async fn get_feeds(&self) -> DbResult<Vec<model::Feed>> {
-        let query = format!("SELECT {} FROM feeds", COLUMNS);
-
-        sqlx::query_as::<_, model::Feed>(&query)
+        sqlx::query_as::<_, model::Feed>("SELECT * FROM feeds")
             .fetch_all(&self.ctx.db)
             .await
             .map_err(|err| err.into())
     }
 
     pub async fn get_feed(&self, uuid: uuid::Uuid) -> DbResult<Option<model::Feed>> {
-        let query = format!("SELECT {} FROM feeds WHERE uuid = ?1", COLUMNS);
-
-        sqlx::query_as::<_, model::Feed>(&query)
+        sqlx::query_as::<_, model::Feed>("SELECT * FROM feeds WHERE uuid = ?1")
             .bind(uuid)
             .fetch_optional(&self.ctx.db)
             .await
@@ -39,23 +33,20 @@ impl FeedRepo {
     }
 
     pub async fn create_feed(&self, data: CreateFeedParams) -> DbResult<model::Feed> {
-        let query = format!(
+        let feed = sqlx::query_as::<_, model::Feed>(
             r#"
-            INSERT INTO feeds ( uuid, title, url, published_at, updated_at )
-            VALUES ( ?1, ?2, ?3, ?4, ?5 )
-            RETURNING {}
+            INSERT INTO feeds (uuid, title, url, published_at, updated_at)
+            VALUES (?1, ?2, ?3, ?4, ?5)
+            RETURNING *
             "#,
-            COLUMNS
-        );
-
-        let feed = sqlx::query_as::<_, model::Feed>(&query)
-            .bind(uuid::Uuid::new_v4())
-            .bind(data.title)
-            .bind(data.url)
-            .bind(data.published_at)
-            .bind(data.updated_at)
-            .fetch_one(&self.ctx.db)
-            .await?;
+        )
+        .bind(uuid::Uuid::new_v4())
+        .bind(data.title)
+        .bind(data.url)
+        .bind(data.published_at)
+        .bind(data.updated_at)
+        .fetch_one(&self.ctx.db)
+        .await?;
 
         Ok(feed)
     }
