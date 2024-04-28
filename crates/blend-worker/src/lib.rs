913b34c2b419b0b69bf7d0e84c747a8e08deb7bc
Zak Nesler <zak@nesler.dev>
2024-04-28 13:54:08 -0400

    tidy

--- crates/blend-worker/src/lib.rs
+++ crates/blend-worker/src/lib.rs
@@ -1,37 +1,6 @@
 pub mod error;
 pub mod job;
-use std::sync::{Arc, Mutex};
+pub mod worker;
 
-use error::WorkerResult;
 pub use job::Job;
-use tokio::sync::mpsc;
-
-pub struct Worker {
-    blend: blend_config::Config,
-    db: sqlx::SqlitePool,
-    recv: Arc<Mutex<mpsc::Receiver<Job>>>,
-}
-
-impl Worker {
-    pub fn new(
-        blend: blend_config::Config,
-        db: sqlx::SqlitePool,
-        recv: mpsc::Receiver<Job>,
-    ) -> Self {
-        Self {
-            blend,
-            db,
-            recv: Arc::new(Mutex::new(recv)),
-        }
-    }
-
-    pub async fn start(&mut self) -> WorkerResult<()> {
-        let mut rx = self.recv.lock().unwrap();
-
-        while let Some(job) = rx.recv().await {
-            dbg!(&job);
-        }
-
-        Ok(())
-    }
-}
+pub use worker::Worker;
---
+++ crates/blend-worker/src/worker.rs
@@ -0,0 +1,33 @@
+use crate::{error::WorkerResult, Job};
+use std::sync::{Arc, Mutex};
+use tokio::sync::mpsc;
+
+pub struct Worker {
+    blend: blend_config::Config,
+    db: sqlx::SqlitePool,
+    recv: Arc<Mutex<mpsc::Receiver<Job>>>,
+}
+
+impl Worker {
+    pub fn new(
+        blend: blend_config::Config,
+        db: sqlx::SqlitePool,
+        recv: mpsc::Receiver<Job>,
+    ) -> Self {
+        Self {
+            blend,
+            db,
+            recv: Arc::new(Mutex::new(recv)),
+        }
+    }
+
+    pub async fn start(&mut self) -> WorkerResult<()> {
+        let mut rx = self.recv.lock().unwrap();
+
+        while let Some(job) = rx.recv().await {
+            dbg!(&job);
+        }
+
+        Ok(())
+    }
+}
