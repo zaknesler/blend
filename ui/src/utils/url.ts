c8ebb7b694cd5b18a3e2823a496cafb770f7dfd1
Zak Nesler <zak@nesler.dev>
2024-04-28 22:27:05 -0400

    fix websocket url prefix

--- ui/src/utils/url.ts
+++ ui/src/utils/url.ts
@@ -10,8 +10,9 @@ export const wsUrl = (uri: string) => {
   const withSlash = uri.startsWith('/') ? uri : `/${uri}`;
   const withApiPrefix = uri.startsWith('/api/ws') ? withSlash : `/api/ws${uri}`;
   const port = location.port === '80' || location.port === '443' ? '' : `:${location.port}`;
+  const prefix = location.protocol.startsWith('https') ? 'wss' : 'ws';
 
   return import.meta.env.PROD
-    ? `ws://${location.hostname}${port}${withApiPrefix}`
-    : `ws://${DEV_HOSTNAME}${withApiPrefix}`;
+    ? `${prefix}://${location.hostname}${port}${withApiPrefix}`
+    : `${prefix}://${DEV_HOSTNAME}${withApiPrefix}`;
 };
