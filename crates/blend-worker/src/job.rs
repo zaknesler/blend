546ff8715446f947c9c4eff2da02f50862e7a75b
Zak Nesler <zak@nesler.dev>
2024-05-01 14:01:23 -0400

    wip

--- crates/blend-worker/src/job.rs
+++ crates/blend-worker/src/job.rs
@@ -1,6 +1,6 @@
 use blend_db::model;
 use serde::Serialize;
-use std::fmt::Display;
+use std::fmt::{format, Display};
 
 #[derive(Debug, Clone, Serialize)]
 #[serde(tag = "type")]
@@ -12,25 +12,20 @@ pub enum Job {
 
 impl Display for Job {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut write_job_str = |name: &str, uuid: uuid::Uuid| {
+            write!(
+                f,
+                "[job: {}] (size = {}) feed = {}",
+                name,
+                std::mem::size_of_val(self),
+                uuid.hyphenated().to_string()
+            )
+        };
+
         match self {
-            Job::RefreshFeed(feed) => write!(
-                f,
-                "[job: refresh feed] (size = {}) feed = {}",
-                std::mem::size_of_val(self),
-                feed.uuid.hyphenated().to_string()
-            ),
-            Job::FetchMetadata(feed) => write!(
-                f,
-                "[job: fetch metadata] (size = {}) feed = {}",
-                std::mem::size_of_val(self),
-                feed.uuid.hyphenated().to_string()
-            ),
-            Job::FetchEntries(feed) => write!(
-                f,
-                "[job: fetch entries] (size = {}) feed = {}",
-                std::mem::size_of_val(self),
-                feed.uuid.hyphenated().to_string()
-            ),
+            Job::RefreshFeed(feed) => write_job_str("refresh feed", feed.uuid),
+            Job::FetchEntries(feed) => write_job_str("fetch entries", feed.uuid),
+            Job::FetchMetadata(feed) => write_job_str("fetch metadata", feed.uuid),
         }
     }
 }
