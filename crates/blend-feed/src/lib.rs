d0b8efaa0aba95b0fb6b64f0b84678ada6daf0a6
Zak Nesler <zak@nesler.dev>
2024-05-20 21:59:51 -0400

    begin feed refactor

--- crates/blend-feed/src/lib.rs
+++ crates/blend-feed/src/lib.rs
@@ -1,39 +1,61 @@
 use error::FeedResult;
+use model::{ParsedEntry, ParsedFeed};
 
 mod error;
+pub use error::FeedError as Error;
 pub mod model;
 
-pub use error::FeedError as Error;
-
-pub async fn parse_feed(url: &str) -> FeedResult<model::ParsedFeed> {
+/// Fetch feed and handle edge cases
+async fn get_feed(url: &str) -> FeedResult<feed_rs::model::Feed> {
     let data = reqwest::get(url).await?.text().await?;
-    let mut feed: model::ParsedFeed = feed_rs::parser::parse(data.as_bytes())
-        .or_else(|err| match err {
-            feed_rs::parser::ParseFeedError::ParseError(_) => todo!(), // fallback to URL and look for feed URL in <head>
-            _ => Err(err),
-        })?
-        .into();
-
-    // TODO: need a nicer way of doing this
-    if feed.url.is_none() {
-        feed.url = Some(url.to_owned());
-    }
+    let feed = feed_rs::parser::parse(data.as_bytes()).or_else(|err| match err {
+        feed_rs::parser::ParseFeedError::ParseError(_) => todo!(), // fallback to URL and look for feed URL in <head>
+        _ => Err(err),
+    })?;
 
     Ok(feed)
 }
 
-pub async fn parse_entries(url: &str) -> FeedResult<Vec<model::ParsedEntry>> {
-    let data = reqwest::get(url).await?.text().await?;
-    let feed = feed_rs::parser::parse(data.as_bytes()).or_else(|err| match err {
-        feed_rs::parser::ParseFeedError::ParseError(_) => todo!(),
-        _ => Err(err),
-    })?;
+// Fetch feed and process the basic feed data
+pub async fn parse_feed(url: &str) -> FeedResult<ParsedFeed> {
+    let feed = get_feed(url).await?;
+
+    let parsed = ParsedFeed {
+        id: feed.id,
+        url: Some(url.to_owned()),
+        title: feed.title.map(|title| title.content),
+        favicon: None,
+        published_at: feed.published,
+        updated_at: feed.updated,
+    };
+
+    Ok(parsed)
+}
+
+/// Fetch feed and process each entry as needed
+pub async fn parse_entries(url: &str) -> FeedResult<Vec<ParsedEntry>> {
+    let feed = get_feed(url).await?;
 
     let entries = feed
         .entries
         .iter()
         .cloned()
-        .map(|entry| entry.into())
+        .map(|entry| {
+            let link = entry
+                .links
+                .iter()
+                .find(|link| link.rel.as_ref().is_some_and(|rel| rel == "self"));
+
+            ParsedEntry {
+                id: entry.id,
+                url: link.map(|link| link.href.clone()), // TODO: normalize this url
+                title: entry.title.map(|title| title.content),
+                summary: entry.summary.map(|summary| summary.content),
+                content_html: entry.content.and_then(|content| content.body), // TODO: normalize and sanitize this
+                published_at: entry.published,
+                updated_at: entry.updated,
+            }
+        })
         .collect::<Vec<model::ParsedEntry>>();
 
     Ok(entries)
--- crates/blend-feed/src/model.rs
+++ crates/blend-feed/src/model.rs
@@ -1,5 +1,6 @@
 use chrono::{DateTime, Utc};
 
+/// Intermediate model for mapping/tweaking feeds from feed-rs.
 #[derive(Debug, Clone)]
 pub struct ParsedFeed {
     pub id: String,
@@ -10,19 +11,7 @@ pub struct ParsedFeed {
     pub updated_at: Option<DateTime<Utc>>,
 }
 
-impl From<feed_rs::model::Feed> for ParsedFeed {
-    fn from(value: feed_rs::model::Feed) -> Self {
-        Self {
-            id: value.id,
-            url: None, // TODO: normalize a way of getting this URL, should fallback sanitized user-submitted URL
-            title: value.title.map(|title| title.content),
-            favicon: None,
-            published_at: value.published,
-            updated_at: value.updated,
-        }
-    }
-}
-
+/// Intermediate model for mapping/tweaking entries from feed-rs.
 #[derive(Debug, Clone)]
 pub struct ParsedEntry {
     pub id: String,
@@ -32,23 +21,4 @@ pub struct ParsedEntry {
     pub content_html: Option<String>,
     pub published_at: Option<DateTime<Utc>>,
     pub updated_at: Option<DateTime<Utc>>,
-}
-
-impl From<feed_rs::model::Entry> for ParsedEntry {
-    fn from(value: feed_rs::model::Entry) -> Self {
-        let link = value
-            .links
-            .iter()
-            .find(|link| link.rel.as_ref().is_some_and(|rel| rel == "self"));
-
-        Self {
-            id: value.id,
-            url: link.map(|link| link.href.clone()), // TODO: normalize this url
-            title: value.title.map(|title| title.content),
-            summary: value.summary.map(|summary| summary.content),
-            content_html: value.content.and_then(|content| content.body), // TODO: normalize and sanitize this
-            published_at: value.published,
-            updated_at: value.updated,
-        }
-    }
 }
