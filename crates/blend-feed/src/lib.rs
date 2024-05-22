4dbd661494e32a5ec7afee7ed2a1db13f21dd1c1
Zak Nesler <zak@nesler.dev>
2024-05-22 19:52:53 -0400

    fix favicon url

--- crates/blend-feed/src/lib.rs
+++ crates/blend-feed/src/lib.rs
@@ -20,12 +20,10 @@ async fn get_feed(url: &str) -> FeedResult<feed_rs::model::Feed> {
 pub async fn parse_feed(url: &str) -> FeedResult<ParsedFeed> {
     let feed = get_feed(url).await?;
 
+    dbg!(&feed);
+
     // Parse favicon URL to use until we can convert the remote image into binary data stored in the db
-    let favicon_url = feed
-        .icon
-        .or_else(|| feed.logo)
-        .map(|image| image.link.map(|link| link.href))
-        .flatten();
+    let favicon_url = feed.icon.or_else(|| feed.logo).map(|image| image.uri);
 
     let parsed = ParsedFeed {
         id: feed.id,
