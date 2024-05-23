b197a10597fabab9e0ca1096151f2ada92b4f5a0
Zak Nesler <zak@nesler.dev>
2024-05-23 13:18:09 -0400

    404 styles

--- ui/src/routes/404.tsx
+++ ui/src/routes/404.tsx
@@ -1,7 +1,21 @@
+import { HiSolidArrowSmallLeft } from 'solid-icons/hi';
+import { Logo } from '~/components/layout/logo';
 import { Panel } from '~/components/layout/panel';
+import { Link } from '~/components/ui/link';
 
 export default () => (
-  <Panel class="flex flex-col gap-4 p-4 md:p-8">
-    <h2>404 Not Found</h2>
-  </Panel>
+  <div class="h-full w-full p-4 md:p-8">
+    <Panel class="flex h-full w-full flex-col items-start gap-8 rounded-lg p-8">
+      <Logo />
+
+      <div class="flex flex-col gap-4">
+        <h2 class="text-4xl font-black">404</h2>
+        <p class="text-gray-500">Page not found.</p>
+        <Link href="/" class="flex items-center gap-2">
+          <HiSolidArrowSmallLeft class="h-6 w-6 text-gray-400" />
+          Home
+        </Link>
+      </div>
+    </Panel>
+  </div>
 );
