753907b90bbb29bae4735581a56194bbda639d06
Zak Nesler <zak@nesler.dev>
2024-06-08 22:54:13 -0400

    truncate feed name

--- ui/src/components/feed/feed-header.tsx
+++ ui/src/components/feed/feed-header.tsx
@@ -7,9 +7,9 @@ type FeedHeaderProps = {
 
 export const FeedHeader: Component<FeedHeaderProps> = props => (
   <div class="flex flex-1 flex-shrink flex-col overflow-hidden whitespace-nowrap">
-    <h2 class="font-semibold">{props.title}</h2>
+    <h2 class="truncate font-semibold">{props.title}</h2>
     <Show when={props.subtitle}>
-      <small class="overflow-hidden overflow-ellipsis text-gray-500 text-xs dark:text-gray-400">{props.subtitle}</small>
+      <small class="truncate text-gray-500 text-xs dark:text-gray-400">{props.subtitle}</small>
     </Show>
   </div>
 );
