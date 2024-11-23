961b611d270be1fb18bc4e478c83d961ffa402dd
Zak Nesler <zak@nesler.dev>
2024-11-23 18:25:20 -0500

    wip

--- ui/src/components/feed/feed-empty-item.tsx
+++ ui/src/components/feed/feed-empty-item.tsx
@@ -9,7 +9,7 @@ export const FeedEmptyItem: Component<FeedEmptyItemProps> = props => {
   return (
     <div
       class={cx(
-        'rounded-md border border-gray-400 border-dashed px-2 py-1.5 text-center text-gray-700 text-xs',
+        'select-none rounded-md border border-gray-400 border-dashed px-2 py-1.5 text-center text-gray-700 text-xs',
         'dark:border-gray-600 dark:text-gray-400',
         props.class,
       )}
